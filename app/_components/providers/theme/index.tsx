c6767ed9cee060ed0888c2374cd916a5b261a5a5
im-luka <lux.9833@gmail.com>
2024-01-21 22:12:00 +0100

    ⚡️ feat: theme setup - other

--- app/_components/providers/theme/index.tsx
+++ app/_components/providers/theme/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FC, ReactNode } from "react";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
@@ -7,6 +9,7 @@ import { colors } from "./colors";
 import { typography } from "./typography";
 import { spacing } from "./spacing";
 import { components } from "./components";
+import { other, resolver } from "./other";
 
 type Props = {
   children: ReactNode;
@@ -17,11 +20,12 @@ const theme = createTheme({
   ...typography,
   spacing,
   components,
+  other,
 });
 
 export const ThemeProvider: FC<Props> = ({ children }) => {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={theme} cssVariablesResolver={resolver}>
       <Notifications
         position="top-right"
         limit={5}
