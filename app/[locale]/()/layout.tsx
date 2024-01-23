51e8f337b90b3d426db62d0aa8fa828050f680d3
im-luka <lux.9833@gmail.com>
2024-01-23 19:19:04 +0100

    ⚡️ feat: navbar

--- app/[locale]/()/layout.tsx
+++ app/[locale]/()/layout.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@mantine/core";
 import type { ReactNode } from "react";
+import { Box } from "@mantine/core";
+import { Navbar } from "@/app/_components/navbar";
 
 type Props = {
   children: ReactNode;
@@ -8,6 +9,7 @@ type Props = {
 export default function HomeLayout({ children }: Props) {
   return (
     <Box w="100%" style={{ border: "1px solid red" }}>
+      <Navbar />
       {children}
     </Box>
   );
--- app/_components/home/home-client.tsx
+++ app/_components/home/home-client.tsx
@@ -1,42 +1,18 @@
 "use client";
 
 import { FC } from "react";
-import { signOut, useSession } from "next-auth/react";
-import { useTranslations } from "next-intl";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { Box, Button, Stack, Title } from "@mantine/core";
-import { logoutMutation } from "@/domain/mutations/logout-mutation";
+import { useQuery } from "@tanstack/react-query";
+import { Center } from "@mantine/core";
 
 export const HomeClient: FC = () => {
-  const t = useTranslations();
-  const { data } = useSession();
-
   const { data: games } = useQuery({
     queryKey: ["games/"],
   });
   console.log(games);
 
-  const { mutateAsync: logout, isPending } = useMutation({
-    mutationFn: logoutMutation.fnc,
-  });
-
-  const handleLogout = async () => {
-    const response = await logout();
-    if (response) {
-      await signOut();
-    }
-  };
-
   return (
-    <Stack>
-      <Box w={300} h={100} bg="primary">
-        <Title>{t("appName")}</Title>
-      </Box>
-      {data?.user && (
-        <Button onClick={handleLogout} loading={isPending}>
-          Log out
-        </Button>
-      )}
-    </Stack>
+    <Center h="100%">
+      <h1>Home Screen</h1>
+    </Center>
   );
 };
