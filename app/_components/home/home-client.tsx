ec97f160da11179f4ef9e518a87f2984499f54f4
im-luka <lux.9833@gmail.com>
2024-01-24 20:03:21 +0100

    ⚡️ feat: create game

--- app/_components/home/home-client.tsx
+++ app/_components/home/home-client.tsx
@@ -1,27 +1,35 @@
 "use client";
 
 import { FC } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { useTranslations } from "next-intl";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { Button, Group, Stack } from "@mantine/core";
+import { IconPlus } from "@tabler/icons-react";
 import {
   gamesQuery,
   gamesQuerySearchParams,
 } from "@/domain/queries/games-query";
 import { GameList } from "@/types/game-list";
 import { GamesTable } from "../games/games-table";
-import { IconPlus } from "@tabler/icons-react";
 import { GAMES_PER_PAGE_DEFAULT_OPTION } from "@/util/constants";
 import { GamesPagination } from "../games/games-pagination";
-import { useSearchParams } from "next/navigation";
-import { useTranslations } from "next-intl";
+import { createGameMutation } from "@/domain/mutations/create-game-mutation";
+import { useNotificationSuccess } from "@/hooks/use-notification-success";
 
 export const HomeClient: FC = () => {
-  const { t, results, total } = useHomeClient();
+  const { t, results, total, isCreatingGame, handleCreateGame } =
+    useHomeClient();
 
   return (
     <Stack p="md">
       <Group justify="space-between">
-        <Button variant="outline" leftSection={<IconPlus size={20} />}>
+        <Button
+          variant="outline"
+          leftSection={<IconPlus size={20} />}
+          loading={isCreatingGame}
+          onClick={handleCreateGame}
+        >
           {t("newAction")}
         </Button>
         <GamesPagination total={total} />
@@ -33,19 +41,35 @@ export const HomeClient: FC = () => {
 
 function useHomeClient() {
   const t = useTranslations("home");
+  const onSuccess = useNotificationSuccess("createGame");
+
   const searchParams = useSearchParams();
   const limit =
     searchParams.get(gamesQuerySearchParams.limit) ??
     GAMES_PER_PAGE_DEFAULT_OPTION;
   const offset = searchParams.get(gamesQuerySearchParams.offset) ?? undefined;
 
-  const { data: games } = useQuery<GameList>({
+  const { data: games, refetch: refetchGames } = useQuery<GameList>({
     queryKey: gamesQuery.key({ limit, offset }),
   });
+
+  const { mutateAsync: createGame, isPending: isCreatingGame } = useMutation({
+    mutationFn: createGameMutation.fnc,
+    onSuccess: async () => {
+      await refetchGames();
+      onSuccess();
+    },
+  });
+
+  const handleCreateGame = async () => {
+    await createGame().catch(() => null);
+  };
 
   return {
     t,
     results: games?.results ?? [],
     total: Math.ceil((games?.count ?? 1) / +limit),
+    isCreatingGame,
+    handleCreateGame,
   };
 }
